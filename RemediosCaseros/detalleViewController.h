963bb1fcee953a31ef95caffa80d2bfe9085c971
Carlos Hernandez <chernandez@somossoftware.com>
2012-03-26 13:01:57 -0600

    creación de los xib De Detalle

---
+++ RemediosCaseros/detalleViewController.h
@@ -0,0 +1,13 @@
+//
+//  detalleViewController.h
+//  RemediosCaseros
+//
+//  Created by User on 3/26/12.
+//  Copyright (c) 2012 Somos Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface detalleViewController : UIViewController
+
+@end
