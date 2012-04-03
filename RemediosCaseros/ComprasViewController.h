0e2d948947bcdaa63fbf5907f6033abba0febc0f
Carlos Hernandez <chernandez@somossoftware.com>
2012-04-03 09:50:02 -0500

    creación de la vista "Compras"

---
+++ RemediosCaseros/ComprasViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComprasViewController.h
+//  RemediosCaseros
+//
+//  Created by User on 4/2/12.
+//  Copyright (c) 2012 Somos Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComprasViewController : UIViewController
+
+@end
