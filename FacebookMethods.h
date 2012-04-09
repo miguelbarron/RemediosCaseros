7b45fc6fd2ea3c97a2b956c59f84dfa6f5aa6ee7
Carlos Hernandez <chernandez@somossoftware.com>
2012-04-09 14:48:06 -0500

    add de archives del sub modulo de Facebook

---
+++ FacebookMethods.h
@@ -0,0 +1,31 @@
+//
+//  FacebookMethods.h
+//  Periodico
+//
+//  Created by Alejandro Diaz on 2/24/12.
+//  Copyright (c) 2012 Somos Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FBConnect.h"
+
+@interface FacebookMethods : NSObject<FBSessionDelegate, FBRequestDelegate, FBDialogDelegate>
+{
+    Facebook *facebook_;
+    BOOL *flag;
+ 
+}
+
+@property (nonatomic,retain)Facebook *facebook;
+
+
++ (FacebookMethods *)sharedInstance;
+-(void)fbSetId;
+-(void)logIn;
+-(void)newFeed:(NSMutableDictionary *)Dic;
+-(void)logOut;
+-(BOOL)logged;
+
+
+
+@end
---
+++ FacebookMethods.m
@@ -0,0 +1,140 @@
+//
+//  FacebookMethods.m
+//  Periodico
+//
+//  Created by Alejandro Diaz on 2/24/12.
+//  Copyright (c) 2012 Somos Software. All rights reserved.
+//
+
+#import "FacebookMethods.h"
+FacebookMethods *sharedFacebook_;
+
+@implementation FacebookMethods
+
+@synthesize facebook = facebook_;
+
+
++ (FacebookMethods *)sharedInstance {
+    if (!sharedFacebook_ ) {
+        sharedFacebook_ = [[FacebookMethods alloc]init];
+    }
+    return sharedFacebook_;
+}
+
+-(void)fbSetId
+{
+ facebook_ = [[Facebook alloc]initWithAppId:@"123176197710962" andDelegate:self];
+
+}
+
+#pragma mark Facebook Methods
+
+-(void)logIn
+{
+    
+    if (![facebook_ isSessionValid]) {
+        NSArray *permissions = [[NSArray alloc] initWithObjects:
+                                @"user_likes", 
+                                @"read_stream",
+                                nil];
+        [facebook_ authorize:permissions];
+        [permissions release];    
+    }
+}
+
+-(BOOL)logged
+{
+    if ([facebook_ isSessionValid]) {
+        NSLog(@"SIIII!");
+        return YES;
+    }
+    else
+    {
+        NSLog(@"Nooo");
+        return NO;
+    }
+
+}
+-(void)newFeed:(NSMutableDictionary *)Dic
+{
+    NSLog(@"FBDiccionario %@",[Dic description]);
+    
+    NSMutableDictionary *Cont = [[NSMutableDictionary alloc]init];
+    
+    NSString *descripcion = [Dic objectForKey:@"description"];
+    NSString *desc;
+    if ([descripcion length]>100) {
+        desc =[NSString stringWithFormat:@"%@ ...", [descripcion substringToIndex:100]];
+    }
+    else
+    {
+        desc = descripcion;
+    }
+    
+    
+    [Cont setObject: desc forKey:@"description"];   
+    [Cont setObject:[Dic objectForKey:@"link"] forKey:@"link"];
+    [Cont setObject:[Dic objectForKey:@"picture"] forKey:@"picture"];
+    [Cont setObject:[Dic objectForKey:@"title"] forKey:@"name"];
+    [Cont setObject:[Dic objectForKey:@"message"] forKey:@"message"];
+    //[Cont setObject: @"http://miRestaurante.com" forKey: @"href"];
+    [Cont setObject:@"Reference" forKey:@"caption"];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    [facebook_ dialog:@"feed"
+            andParams:Cont
+          andDelegate:self];
+    
+    //    [facebook_ dialog:@"stream.publish" andParams:Cont andDelegate:self];
+    
+    [Cont release];
+}
+
+
+
+-(void)logOut
+{
+    [facebook_ logout];
+    [self fbDidLogout];
+    flag = NO;
+}
+
+- (void) fbDidLogout {
+    // Remove saved authorization information if it exists
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if ([defaults objectForKey:@"FBAccessTokenKey"]) {
+        [defaults removeObjectForKey:@"FBAccessTokenKey"];
+        [defaults removeObjectForKey:@"FBExpirationDateKey"];
+        [defaults synchronize];
+    }
+}
+
+// Pre 4.2 support
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+    return [facebook_ handleOpenURL:url]; 
+}
+
+// For 4.2+ support
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [facebook_ handleOpenURL:url]; 
+}
+
+- (void)fbDidLogin {
+
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:[facebook_ accessToken] forKey:@"FBAccessTokenKey"];
+    [defaults setObject:[facebook_ expirationDate] forKey:@"FBExpirationDateKey"];
+    [defaults synchronize];
+    flag =YES;     
+}
+
+@end
