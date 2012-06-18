df513133d9c72040091125b096ec4f3c90bfe307
Alejandro Diaz <adiaz@somossoftware.com>
2012-06-18 13:42:04 -0500

    se agregaron clases para ver vídeos

---
+++ RemediosCaseros/VideoPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoPlayerViewController.h
+//  RemediosCaseros
+//
+//  Created by Alex Diaz on 18/06/2012.
+//  Copyright (c) 2012 Somos Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoPlayerViewController : UIViewController
+
+@end
